5bdfbcc87bda107d4be47a6f85a992e66d333896
AhmedKECHIR <159757634+AhmedKECHIR@users.noreply.github.com>
2024-02-12 10:44:15 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -30,9 +30,6 @@ def meteo():
 def mongraphique():
     return render_template("graphique.html")
   
-@app.route("/histogramme/")
-def mongraphique():
-    return render_template("histogramme.html")
   
 if __name__ == "__main__": #commentaires
   app.run(debug=True)
